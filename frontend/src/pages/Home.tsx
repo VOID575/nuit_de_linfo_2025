5a548464e0bfaba7f25128754a71e0209b6ea7ba
justin <jgerbaud@etudiant.univ-lr.fr>
2025-12-05 00:44:00 +0100

    home était pas ouf

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,42 +4,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>Accueil</h1>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
-      <p>Bienvenue sur la page d'accueil de notre site !</p>
-      <p>Découvrez nos services et explorez notre contenu.</p>
+      
       
     </div>
   );
