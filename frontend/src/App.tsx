b5a87f4f601c3b757386ac3579c0e707f170c219
justin <jgerbaud@etudiant.univ-lr.fr>
2025-12-05 01:37:56 +0100

    feat(front):pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/explorer" element={<Explorer />} />
+            <Route path="/index.html" element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
